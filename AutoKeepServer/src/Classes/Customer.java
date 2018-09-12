a6e7cb62ac9f7b8771caecc12a64eeabbee3219e
Shahak Shaked <Shahak.Shaked@gmail.com>
2018-09-12 11:04:59 +0300

    1

--- AutoKeepServer/src/Classes/Customer.java
+++
@@ -1,8 +0,0 @@
-package Classes;
-
-public class Customer extends User{
-	
-	public Customer(String userName, String password,String dateOfBirth,String firstName,String lastName){
-		super(userName, password, dateOfBirth, firstName, lastName);
-	}
-}
